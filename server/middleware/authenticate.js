83fe1776409632c68908a80f88558c38a2088581
Sigita <sigita@jalenskaite.info>
2018-06-17 22:25:11 +0200

    Add middleware authenticate

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,18 @@
+import {User} from './../models/user'
+
+const authenticate = (req, res, next) => {
+  const token = req.header('x-auth')
+  User.findByToken(token).then((user) => {
+    if (!user) {
+      res.status(404).send()
+    }
+
+    req.user = user
+    req.token = token
+    next()
+  }).catch((e) => {
+    res.status(401).send(e)
+  })
+}
+
+export {authenticate}
